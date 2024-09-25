2ccc5a6d14e2c251b957e00f3fa6d68b860aaf19
cotete <feliperibeirots@gmail.com>
2024-09-25 18:37:41 -0300

    comentarios

--- src/br/com/fiap/controller/FilmeController.java
+++ src/br/com/fiap/controller/FilmeController.java
@@ -9,55 +9,67 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class FilmeController {
-    public String inserirFilme(String titulo,String genero,String produtora) throws ClassNotFoundException, SQLException {
-        String resultado;
-        Connection con = ConnectionFactory.abrirConexao();
-        Filme filme = new Filme();
-        filme.setTitulo(titulo);
-        filme.setGenero(genero);
-        filme.setProdutora(produtora);
-        FilmeDAO dao = new FilmeDAO(con);
-        resultado = dao.inserir(filme);
-        ConnectionFactory.fecharConexao(con);
-        return resultado;
+
+    // Método para inserir um novo filme no banco de dados
+    public String inserirFilme(String titulo, String genero, String produtora) throws ClassNotFoundException, SQLException {
+        String resultado;  // Variável para armazenar o resultado da operação
+        Connection con = ConnectionFactory.abrirConexao();  // Abre uma conexão com o banco de dados
+        Filme filme = new Filme();  // Cria um novo objeto Filme
+        filme.setTitulo(titulo);  // Define o título do filme
+        filme.setGenero(genero);  // Define o gênero do filme
+        filme.setProdutora(produtora);  // Define a produtora do filme
+        FilmeDAO dao = new FilmeDAO(con);  // Cria um DAO (Data Access Object) para manipulação do banco de dados
+        resultado = dao.inserir(filme);  // Chama o método para inserir o filme no banco de dados e armazena o resultado
+        ConnectionFactory.fecharConexao(con);  // Fecha a conexão com o banco de dados
+        return resultado;  // Retorna o resultado da operação
     }
-    public String alterarFilme(int codigo,String titulo,String genero,String produtora) throws ClassNotFoundException, SQLException {
-        String resultado;
-        Connection con = ConnectionFactory.abrirConexao();
-        Filme filme = new Filme();
-        filme.setGenero(genero);
-        filme.setTitulo(titulo);
-        filme.setCodigo(codigo);
-        filme.setProdutora(produtora);
-        FilmeDAO dao = new FilmeDAO(con);
-        resultado = dao.alterar(filme);
-        ConnectionFactory.fecharConexao(con);
-        return resultado;
+
+    // Método para alterar um filme existente no banco de dados
+    public String alterarFilme(int codigo, String titulo, String genero, String produtora) throws ClassNotFoundException, SQLException {
+        String resultado;  // Variável para armazenar o resultado da operação
+        Connection con = ConnectionFactory.abrirConexao();  // Abre uma conexão com o banco de dados
+        Filme filme = new Filme();  // Cria um novo objeto Filme
+        filme.setCodigo(codigo);  // Define o código do filme (para identificar qual filme alterar)
+        filme.setTitulo(titulo);  // Define o novo título do filme
+        filme.setGenero(genero);  // Define o novo gênero do filme
+        filme.setProdutora(produtora);  // Define a nova produtora do filme
+        FilmeDAO dao = new FilmeDAO(con);  // Cria um DAO para manipulação do banco de dados
+        resultado = dao.alterar(filme);  // Chama o método para alterar o filme no banco de dados e armazena o resultado
+        ConnectionFactory.fecharConexao(con);  // Fecha a conexão com o banco de dados
+        return resultado;  // Retorna o resultado da operação
     }
+
+    // Método para excluir um filme do banco de dados
     public String excluirFilme(int codigo) throws ClassNotFoundException, SQLException {
-        String resultado;
-        Connection con = ConnectionFactory.abrirConexao();
-        Filme filme = new Filme();
-        filme.setCodigo(codigo);
-        FilmeDAO dao = new FilmeDAO(con);
-        resultado = dao.excluir(filme);
-        ConnectionFactory.fecharConexao(con);
-        return resultado;
+        String resultado;  // Variável para armazenar o resultado da operação
+        Connection con = ConnectionFactory.abrirConexao();  // Abre uma conexão com o banco de dados
+        Filme filme = new Filme();  // Cria um novo objeto Filme
+        filme.setCodigo(codigo);  // Define o código do filme (para identificar qual filme excluir)
+        FilmeDAO dao = new FilmeDAO(con);  // Cria um DAO para manipulação do banco de dados
+        resultado = dao.excluir(filme);  // Chama o método para excluir o filme no banco de dados e armazena o resultado
+        ConnectionFactory.fecharConexao(con);  // Fecha a conexão com o banco de dados
+        return resultado;  // Retorna o resultado da operação
     }
+
+    // Método para listar todos os filmes do banco de dados
     public String listarFilmes() throws ClassNotFoundException, SQLException {
-        String resultado = "";
-        Connection con = ConnectionFactory.abrirConexao();
-        ArrayList<Filme> listaFilmes = new ArrayList<Filme>();
-        FilmeDAO dao = new FilmeDAO(con);
-        listaFilmes = dao.listarTodos();
-        if (listaFilmes != null){
-            for (Filme filme : listaFilmes){
-                resultado += "Codigo: "+filme.getCodigo()+"\nTitulo: " +filme.getTitulo()+"\nGenero: "+filme.getGenero()+"\nProdutora: " + filme.getProdutora()+"\n";
+        String resultado = "";  // Variável para armazenar o resultado da operação
+        Connection con = ConnectionFactory.abrirConexao();  // Abre uma conexão com o banco de dados
+        ArrayList<Filme> listaFilmes = new ArrayList<Filme>();  // Cria uma lista para armazenar os filmes
+        FilmeDAO dao = new FilmeDAO(con);  // Cria um DAO para manipulação do banco de dados
+        listaFilmes = dao.listarTodos();  // Chama o método para listar todos os filmes e armazena-os na lista
+
+        // Verifica se a lista de filmes não está vazia
+        if (listaFilmes != null) {
+            // Itera sobre a lista de filmes e concatena as informações de cada filme na variável 'resultado'
+            for (Filme filme : listaFilmes) {
+                resultado += "Codigo: " + filme.getCodigo() + "\nTitulo: " + filme.getTitulo() + "\nGenero: " + filme.getGenero() + "\nProdutora: " + filme.getProdutora() + "\n";
             }
-            ConnectionFactory.fecharConexao(con);
-            return resultado;
+            ConnectionFactory.fecharConexao(con);  // Fecha a conexão com o banco de dados
+            return resultado;  // Retorna a lista de filmes formatada como uma string
         }
-        ConnectionFactory.fecharConexao(con);
-        return "Não há filmes para listar.";
+
+        ConnectionFactory.fecharConexao(con);  // Fecha a conexão com o banco de dados
+        return "Não há filmes para listar.";  // Retorna uma mensagem caso não haja filmes para listar
     }
 }
--- src/br/com/fiap/model/dao/FilmeDAO.java
+++ src/br/com/fiap/model/dao/FilmeDAO.java
@@ -9,68 +9,91 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class FilmeDAO {
-    private Connection con;
+    private Connection con; // Atributo para armazenar a conexão com o banco de dados
 
+    // Construtor da classe que recebe uma conexão como parâmetro
     public FilmeDAO(Connection con) {
         this.con = con;
     }
 
+    // Método para obter a conexão
     public Connection getCon() {
         return con;
     }
 
-    public String inserir(Filme filme){
+    // Método para inserir um filme no banco de dados
+    public String inserir(Filme filme) {
+        // Instrução SQL para inserir os dados do filme
         String sql = "insert into ddd_filme(titulo, genero, produtora) values(?, ?, ?)";
-        try (PreparedStatement ps = getCon().prepareStatement(sql)){
-            ps.setString(1,filme.getTitulo());
-            ps.setString(2,filme.getGenero());
-            ps.setString(3,filme.getProdutora());
-            if (ps.executeUpdate()>0){
+        try (PreparedStatement ps = getCon().prepareStatement(sql)) { // Prepara a instrução SQL
+            // Define os parâmetros da instrução SQL
+            ps.setString(1, filme.getTitulo());
+            ps.setString(2, filme.getGenero());
+            ps.setString(3, filme.getProdutora());
+
+            // Executa a instrução e verifica se houve inserção
+            if (ps.executeUpdate() > 0) {
                 return "Inserido com sucesso.";
-            }else{
+            } else {
                 return "Falha ao inserir.";
             }
         } catch (SQLException e) {
-            return "erro de sql" + e.getMessage();
+            return "erro de sql: " + e.getMessage(); // Retorna mensagem de erro em caso de falha
         }
     }
 
-    public String alterar(Filme filme){
+    // Método para alterar um filme no banco de dados
+    public String alterar(Filme filme) {
+        // Instrução SQL para atualizar os dados do filme
         String sql = "update ddd_filme set titulo = ?, genero = ?, produtora = ? where codigo = ?";
-        try (PreparedStatement ps = getCon().prepareStatement(sql)){
-            ps.setString(4,String.valueOf(filme.getCodigo()));
-            ps.setString(1,filme.getTitulo());
-            ps.setString(2,filme.getGenero());
-            ps.setString(3,filme.getProdutora());
-            if (ps.executeUpdate()>0){
-                return "alterado com sucesso.";
-            }else{
+        try (PreparedStatement ps = getCon().prepareStatement(sql)) { // Prepara a instrução SQL
+            // Define os parâmetros da instrução SQL
+            ps.setString(1, filme.getTitulo());
+            ps.setString(2, filme.getGenero());
+            ps.setString(3, filme.getProdutora());
+            ps.setString(4, String.valueOf(filme.getCodigo()));
+
+            // Executa a instrução e verifica se houve alteração
+            if (ps.executeUpdate() > 0) {
+                return "Alterado com sucesso.";
+            } else {
                 return "Falha ao alterar.";
             }
         } catch (SQLException e) {
-            return "erro de sql" + e.getMessage();
+            return "erro de sql: " + e.getMessage(); // Retorna mensagem de erro em caso de falha
         }
     }
-    public String excluir(Filme filme){
+
+    // Método para excluir um filme do banco de dados
+    public String excluir(Filme filme) {
+        // Instrução SQL para excluir um filme com base no código
         String sql = "delete from ddd_filme where codigo = ?";
-        try (PreparedStatement ps = getCon().prepareStatement(sql)){
-            ps.setString(1,String.valueOf(filme.getCodigo()));
-            if (ps.executeUpdate()>0){
-                return "excluido com sucesso.";
-            }else{
+        try (PreparedStatement ps = getCon().prepareStatement(sql)) { // Prepara a instrução SQL
+            // Define o parâmetro da instrução SQL
+            ps.setString(1, String.valueOf(filme.getCodigo()));
+
+            // Executa a instrução e verifica se houve exclusão
+            if (ps.executeUpdate() > 0) {
+                return "Excluído com sucesso.";
+            } else {
                 return "Falha ao excluir.";
             }
         } catch (SQLException e) {
-            return "erro de sql" + e.getMessage();
+            return "erro de sql: " + e.getMessage(); // Retorna mensagem de erro em caso de falha
         }
     }
-    public ArrayList<Filme> listarTodos(){
+
+    // Método para listar todos os filmes do banco de dados
+    public ArrayList<Filme> listarTodos() {
+        // Instrução SQL para selecionar todos os filmes ordenados por código
         String sql = "select * from ddd_filme order by codigo";
-        ArrayList<Filme> listaFilme = new ArrayList<Filme>();
-        try (PreparedStatement ps = getCon().prepareStatement(sql)){
-            ResultSet rs = ps.executeQuery(sql);
-            if (rs != null){
-                while (rs.next()){
+        ArrayList<Filme> listaFilme = new ArrayList<Filme>(); // Lista para armazenar os filmes
+
+        try (PreparedStatement ps = getCon().prepareStatement(sql)) { // Prepara a instrução SQL
+            ResultSet rs = ps.executeQuery(sql); // Executa a consulta e obtém o resultado
+            if (rs != null) {
+                // Itera sobre o resultado e adiciona cada filme à lista
+                while (rs.next()) {
                     Filme filme = new Filme();
                     filme.setCodigo(Integer.parseInt(rs.getString(1)));
                     filme.setTitulo(rs.getString(2));
@@ -78,13 +101,12 @@ public class FilmeDAO {
                     filme.setProdutora(rs.getString(4));
                     listaFilme.add(filme);
                 }
-                return listaFilme;
-            }else{
-                return null;
+                return listaFilme; // Retorna a lista de filmes
+            } else {
+                return null; // Retorna null se não houver resultados
             }
-
         } catch (SQLException e) {
-            System.out.println("erro de sql" + e.getMessage());
+            System.out.println("erro de sql: " + e.getMessage()); // Exibe mensagem de erro em caso de falha
             return null;
         }
     }
